d729f4ceed56edb1da12319b5222d0212a27b397
mrgos <sabbatwonka@hotmail.com>
2026-02-18 18:50:06 -0600

    reorganize

--- api/NoteApi/obj/Debug/net10.0/NoteApi.AssemblyInfo.cs
+++ api/NoteApi/obj/Debug/net10.0/NoteApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b69dafb97ebbd21408328865b383375add2c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
